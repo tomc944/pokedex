3b2da69f8f7dddbd46d12fccc58e5e9eacf0f34a
App Academy Student <student@appacademy.io>
2015-12-10 14:29:12 -0800

    react routers with app component

---
+++ frontend/components/app.jsx
@@ -0,0 +1,16 @@
+var React = require('react'),
+    PokemonsIndex = require('./pokemons/pokemonsIndex.jsx');
+
+var App = React.createClass({
+  render: function () {
+    return(
+      <div id="pokedex">
+        <div className="pokemon-index-pane">
+          <PokemonsIndex />
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = App;
--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,11 +1,18 @@
 var React = require('react'),
     ReactDOM = require('react-dom'),
-    PokemonsIndex = require('./components/pokemons/pokemonsIndex');
+    PokemonsIndex = require('./components/pokemons/pokemonsIndex'),
+    Router = require('react-router').Router,
+    Route = require('react-router').Route,
+    App = require('./components/app.jsx');
 
 // window.apiUtil = require('./util/apiUtil.js');
 // window.PokemonStore = require('./stores/pokemon.js');
 
 window.addEventListener('DOMContentLoaded', function() {
   var root = document.getElementById('root');
-  ReactDOM.render(<PokemonsIndex />, root);
+  ReactDOM.render((
+    <Router>
+      <Route path="/" component={App} />
+    </Router>)
+  , root);
 });
